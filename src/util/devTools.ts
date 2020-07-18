4f8bca1232d821d0a7926614b40476e58c12c02b
michael-mml <mml.liu@mail.utoronto.ca>
2020-07-18 18:19:30 -0400

    Add dynamic import for electron-devtools-installer

--- src/util/devTools.ts
+++ src/util/devTools.ts
@@ -3,16 +3,15 @@ import { isDevelopmentMode } from './isDevelopmentMode';
 
 const ns = '[devTools]';
 
-// electron-devtools-installer issue: https://github.com/alexdevero/electron-react-webpack-boilerplate/issues/16
 export const installDevTools = async (app: Electron.App) => {
   const fn = '[install]';
+  // Since electron-devtools-installer is a devDependency, it is not included when the app is packaged
+  // Related issue: https://github.com/alexdevero/electron-react-webpack-boilerplate/issues/16
   if (isDevelopmentMode(app)) {
-    const {
-      default: installExtension,
-      VUEJS_DEVTOOLS,
-    } = require('electron-devtools-installer');
+    const module = await import('electron-devtools-installer');
+    const installExtension = module.default;
     try {
-      const name = await installExtension([VUEJS_DEVTOOLS]);
+      const name = await installExtension([module.VUEJS_DEVTOOLS]);
       log.info(`${ns}${fn} Added extension: ${name}`);
     } catch (err) {
       log.error(`${ns}${fn} Failed to install extension: ${name}`);
