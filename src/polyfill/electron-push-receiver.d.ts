3f73d2588d10edc495808bf16bd7ccff843af701
michael-mml <mml.liu@mail.utoronto.ca>
2020-07-19 21:15:56 -0400

    Change indentation of .d.ts

--- src/polyfill/electron-push-receiver.d.ts
+++ src/polyfill/electron-push-receiver.d.ts
@@ -1,8 +1,8 @@
 // Type declarations for electron-push-receiver
 declare module 'electron-push-receiver/src/constants' {
-    export const START_NOTIFICATION_SERVICE: string;
-    export const NOTIFICATION_SERVICE_STARTED: string;
-    export const NOTIFICATION_SERVICE_ERROR: string
-    export const NOTIFICATION_RECEIVED: string;
-    export const TOKEN_UPDATED: string;
+  export const START_NOTIFICATION_SERVICE: string;
+  export const NOTIFICATION_SERVICE_STARTED: string;
+  export const NOTIFICATION_SERVICE_ERROR: string
+  export const NOTIFICATION_RECEIVED: string;
+  export const TOKEN_UPDATED: string;
 }
